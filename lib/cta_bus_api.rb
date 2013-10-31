b876b0bd81a88d62cfd4060d10fef612c4aaebf2
Abejide Ayodele <abejideayodele@gmail.com>
2013-10-30 19:06:38 -0500

    routes and time methods created

--- lib/cta_bus_api.rb
+++ lib/cta_bus_api.rb
@@ -4,15 +4,22 @@ require "open-uri"
 class CtaBusApi
 
   def initialize(key: you_must_supply_a_key)
-    response = open("http://localhost:9292/bustime/api/v1/gettime")
-    @parsed_response = Nokogiri.parse(response)
   end
 
   def current_time
+    call_url "gettime"
     parsed_response.xpath("//tm").text
   end
 
-  private
+  def routes
+    call_url "getroutes"
+    parsed_response.xpath("//route//rtnm").map(&:text)
+  end
 
+  private
+  def call_url extension
+    response = open("http://localhost:9292/bustime/api/v1/#{extension}")
+    @parsed_response = Nokogiri.parse(response)
+  end
   attr_reader :parsed_response
 end
--- test_api_server/app.rb
+++ test_api_server/app.rb
@@ -6,3 +6,9 @@ get "/bustime/api/v1/gettime" do
   content_type "text/xml"
   send_file "index.xml"
 end
+
+# http://www.ctabustracker.com/bustime/api/v1/getroutes
+get "/bustime/api/v1/getroutes" do
+  content_type "text/xml"
+  send_file "get_routes.xml"
+end
